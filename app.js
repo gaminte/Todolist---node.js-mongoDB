0bba99beda0602dd783eb94d7a254c309f4e6e53
Subramania Santhosh <subramaniasanthosh@gmail.com>
2023-07-20 17:25:41 +0530

    Update app.js
    fix: add tcp port

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const lodash = require("lodash");
-const date = require(__dirname + "/date.js");
 
+const port = process.env.PORT || 3000
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -72,6 +72,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
